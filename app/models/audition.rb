b8b28248c2778275b32cbe83aec7f58f6341aca5
Adam La Rosa <adam.larosa@flatironschool.com>
2022-03-01 07:58:55 -0800

    Revert "solution"
    This reverts commit dd319c45ca1aa1e34f790f1197a217fd33463c4a.

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,12 +1,3 @@
 class Audition < ActiveRecord::Base
-    belongs_to :role
-
-    def call_back
-
-        # self.hired = ture
-        # self.save
-
-        self.update(hired: true)
-    end
-
+  
 end
