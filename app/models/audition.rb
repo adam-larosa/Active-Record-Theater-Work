69c8e9cc1abfec3386eef7fec138438dfe2101b8
Adam La Rosa <adam.larosa@flatironschool.com>
2022-08-16 08:20:00 -0700

    Revert "011022 solution"
    This reverts commit a41b1d97676e916f0c3e01e1f5f604b73dc7e8f4.

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,17 +1,3 @@
 class Audition < ActiveRecord::Base
-    belongs_to :role
-
-
-    # Audition#call_back will change the the hired attribute to true
-
-    def call_back
-        # self.hired = true
-        # self.save
-
-        self.update(hired: true)
-
-    end
-
+  
 end
-
-
--- db/migrate/20220301160349_create_roles.rb
+++
@@ -1,9 +0,0 @@
-class CreateRoles < ActiveRecord::Migration[5.2]
-  def change
-
-    create_table( :roles ) {|t|
-      t.string( :character_name )
-    }
-
-  end
-end
